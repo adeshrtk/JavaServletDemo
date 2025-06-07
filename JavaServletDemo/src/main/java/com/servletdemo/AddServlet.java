0e41067b7e8e81d5f4340fae3326e96a4b2c9549
adeshrtk <adeshnyc2018@hotmail.com>
2025-06-07 14:58:40 +0530

    RequestDispatcher

--- JavaServletDemo/src/main/java/com/servletdemo/AddServlet.java
+++ JavaServletDemo/src/main/java/com/servletdemo/AddServlet.java
@@ -3,6 +3,8 @@ package com.servletdemo;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -27,8 +29,21 @@ public class AddServlet extends HttpServlet {
 		
 		int k = i + j;
 		
-		PrintWriter out = res.getWriter();		
-		
+		PrintWriter out = res.getWriter();
 		out.println("The sum is: " + k);
+		
+	}
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i + j;
+		
+		req.setAttribute("k", k);
+		
+		//RequestDispatcher
+		RequestDispatcher rd = req.getRequestDispatcher("sq");
+		rd.forward(req, res);
 	}
 }
