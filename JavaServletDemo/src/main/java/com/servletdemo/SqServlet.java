d3deeede7755dc998b0aeaecb1192d1ba9ada805
adeshrtk <adeshnyc2018@hotmail.com>
2025-06-08 15:41:49 +0530

    @WebServlet annotation

--- JavaServletDemo/src/main/java/com/servletdemo/SqServlet.java
+++ JavaServletDemo/src/main/java/com/servletdemo/SqServlet.java
@@ -5,12 +5,14 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet("/sq")
 public class SqServlet extends HttpServlet {
 
 //	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
@@ -41,18 +43,18 @@ public class SqServlet extends HttpServlet {
 			PrintWriter out = res.getWriter();
 			out.println("Result is : " + k);
 			
-			out.println("Hi ");
-			
-			ServletContext ctx = getServletContext();
-			String name = ctx.getInitParameter("name");
-			out.println(name);
-			
-			String phone = ctx.getInitParameter("phone");
-			out.println(phone);			
-			
-			ServletConfig cg = getServletConfig();
-			String str = cg.getInitParameter("servletname");
-			out.println(str);
+//			out.println("Hi ");
+//			
+//			ServletContext ctx = getServletContext();
+//			String name = ctx.getInitParameter("name");
+//			out.println(name);
+//			
+//			String phone = ctx.getInitParameter("phone");
+//			out.println(phone);			
+//			
+//			ServletConfig cg = getServletConfig();
+//			String str = cg.getInitParameter("servletname");
+//			out.println(str);
 		
 			System.out.println("Sq called");
 	}	
