0a72822ee90dd447f5b007db2d8b68b828e4d6fc
adeshrtk <adeshnyc2018@hotmail.com>
2025-06-08 13:24:51 +0530

    Servlet session and cookies

--- JavaServletDemo/src/main/java/com/servletdemo/AddServlet.java
+++ JavaServletDemo/src/main/java/com/servletdemo/AddServlet.java
@@ -5,9 +5,11 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet {
 
@@ -51,6 +53,12 @@ public class AddServlet extends HttpServlet {
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i + j;
+		
+//		HttpSession session = req.getSession();
+//		session.setAttribute("k", k);
+		
+		Cookie cookie = new Cookie("k", k + "");
+		res.addCookie(cookie);
 		
 		res.sendRedirect("sq?k=" + k);
 	}
--- JavaServletDemo/src/main/java/com/servletdemo/SqServlet.java
+++ JavaServletDemo/src/main/java/com/servletdemo/SqServlet.java
@@ -3,9 +3,11 @@ package com.servletdemo;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SqServlet extends HttpServlet {
 
@@ -19,15 +21,24 @@ public class SqServlet extends HttpServlet {
 //		out.println("Square of No is = " + k);
 //	}
 	
-public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+			
+			//int k = Integer.parseInt(req.getParameter("k"));
+//			HttpSession session = req.getSession();
+//			int k = (int)session.getAttribute("k");
 		
-		int k = Integer.parseInt(req.getParameter("k"));
+			int k = 0;
+			Cookie cookies[] = req.getCookies();
+			for(Cookie c : cookies) {
+				if(c.getName().equals("k"))
+					k = Integer.parseInt(c.getValue());
+			}
+			
+			k = k * k;
+			
+			PrintWriter out = res.getWriter();
+			out.println("Result is : " + k);
 		
-		k = k * k;
-		
-		PrintWriter out = res.getWriter();
-		out.println("Result is : " + k);
-	
-		System.out.println("Sq called");
-	}
+			System.out.println("Sq called");
+		}
 }
